83992851bfdcbf1f122c1abc06cf4c68a7b4a0d7
Anna Zinchenko <zinchenkoanna205@gmail.com>
2025-11-05 22:14:07 +0100

    grammar.pest updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
-pub fn placeholder() {}
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "./grammar.pest"]
+pub struct SrtSubtitleParser;
+
+pub fn parse_index(input: &str) -> Result<u32, pest::error::Error<Rule>> {
+    let pairs = SrtSubtitleParser::parse(Rule::index, input)?;
+    
+    let index_pair = pairs.into_iter().next().unwrap();
+
+    let index_value = index_pair.as_str().parse::<u32>().unwrap();
+    
+    Ok(index_value)
+}
