f0915285d04dbda12b9e9322b282c0107e85fc59
Anna Zinchenko <zinchenkoanna205@gmail.com>
2025-11-11 14:18:09 +0100

    tests added

--- tests/grammar_tests.rs
+++ tests/grammar_tests.rs
@@ -1,12 +1,60 @@
-use anyhow::anyhow;
 use srt_subtitles_parser::*;
-use pest::Parser;
+use anyhow::Result;
+
+const SINGLE_BLOCK: &str = "26
+00:02:13,383 --> 00:02:14,883
+Carol moved her stuff out today.";
+
+const MULTI_LINE_BLOCK: &str = "28
+00:02:16,302 --> 00:02:18,095
+- Let me get you some coffee.
+- Thanks.
+";
+
+const FULL_FILE: &str = "55
+00:03:52,440 --> 00:03:54,900
+This is everybody.
+This is Chandler and Phoebe...
+
+56
+00:03:55,109 --> 00:03:57,361
+...and Joey. And remember
+my brother, Ross?";
 
 #[test]
-fn index_single_digit_test() -> anyhow::Result<()> {
-    let pair = SrtSubtitleParser::parse(Rule::index, "82")?
-        .next()
-        .ok_or_else(|| anyhow!("no pair"))?;
-    assert_eq!(pair.as_str(), "82");
+fn test_parse_index() -> Result<()> {
+    let subtitle = parse_subtitle_block(SINGLE_BLOCK)?;
+    assert_eq!(subtitle.index, 26);
+    Ok(())
+}
+
+#[test]
+fn test_parse_timecode() -> Result<()> {
+    let subtitle = parse_subtitle_block(SINGLE_BLOCK)?;
+    assert_eq!(subtitle.start_time, "00:02:13,383");
+    assert_eq!(subtitle.end_time, "00:02:14,883");
+    Ok(())
+}
+
+#[test]
+fn test_parse_text_single_line() -> Result<()> {
+    let subtitle = parse_subtitle_block(SINGLE_BLOCK)?;
+    assert_eq!(subtitle.text, vec!["Carol moved her stuff out today."]);
+    Ok(())
+}
+
+#[test]
+fn test_parse_text_multi_line() -> Result<()> {
+    let subtitle = parse_subtitle_block(MULTI_LINE_BLOCK)?;
+    assert_eq!(subtitle.text, vec!["- Let me get you some coffee.", "- Thanks."]);
+    Ok(())
+}
+
+#[test]
+fn test_parse_file_multiple_blocks() -> Result<()> {
+    let subtitles = parse_file(FULL_FILE)?;
+    assert_eq!(subtitles.len(), 2);
+    assert_eq!(subtitles[0].index, 55);
+    assert_eq!(subtitles[1].index, 56);
     Ok(())
 }
